6b2c55e6ba4e2706197f3cebaee1e751f2684e8c
Loki314159 <bjpye314@gmail.com>
2025-09-06 00:08:38 +1000

    there is now a visible week within the gui

--- overlay.py
+++ overlay.py
@@ -1,53 +1,48 @@
 import tkinter as tk
-from tkcalendar import Calendar, DateEntry
 
-class TimetableApp:
-    def __init__(self, master):
-        self.master = master
-        master.title("Timetable Application")
+class event:
+    pass
 
-        # Calendar Widget
-        self.cal = Calendar(master, selectmode='day', year=2025, month=9, day=1)
-        self.cal.pack(pady=20)
-
-        # Event Input
-        self.event_label = tk.Label(master, text="Event Description:")
-        self.event_label.pack()
-        self.event_entry = tk.Entry(master, width=50)
-        self.event_entry.pack()
-
-        # Add Event Button
-        self.add_button = tk.Button(master, text="Add Event", command=self.add_event)
-        self.add_button.pack(pady=10)
-
-        # Event List Display
-        self.event_list_label = tk.Label(master, text="Events for Selected Date:")
-        self.event_list_label.pack()
-        self.event_list = tk.Listbox(master, height=10, width=50)
-        self.event_list.pack()
-
-        # Bind calendar selection to update event list
-        self.cal.bind("<<CalendarSelected>>", self.update_event_list)
-
-        self.events = {} # Dictionary to store events: {date_str: [event1, event2]}
-
-    def add_event(self):
-        selected_date = self.cal.get_date()
-        event_description = self.event_entry.get()
-        if event_description:
-            if selected_date not in self.events:
-                self.events[selected_date] = []
-            self.events[selected_date].append(event_description)
-            self.update_event_list(None) # Update the listbox
-            self.event_entry.delete(0, tk.END) # Clear input field
-
-    def update_event_list(self, event):
-        self.event_list.delete(0, tk.END)
-        selected_date = self.cal.get_date()
-        if selected_date in self.events:
-            for item in self.events[selected_date]:
-                self.event_list.insert(tk.END, item)
+days = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday'
+]
 
 root = tk.Tk()
-app = TimetableApp(root)
+root.title('Week View')
+
+for i in range(24):
+    root.rowconfigure(i, weight=1)
+root.columnconfigure([1, 2, 3, 4, 5, 6, 7], weight=3)
+root.columnconfigure([0], weight=1)
+frames = {}
+
+for index, day in enumerate(days):
+    frame = tk.Frame(root,
+                          relief="raised",
+                          borderwidth=2,
+                          highlightcolor='lightblue'
+                          )
+    frame.grid(row=1,
+               column=index + 1,
+               padx=2, pady=2,
+               sticky='news',
+               rowspan=24)
+    frames[day] = frame
+    daylabel = tk.Label(text=day.capitalize(), master=root)
+    daylabel.grid(column=index + 1, row=0)
+
+for i in range(24):
+    if i > 9:
+        timefuni = tk.Label(text=f'{i}:00', master=root)
+        timefuni.grid(row=i + 1, column=0)
+    else:
+        timefuni = tk.Label(text=f'0{i}:00', master=root)
+        timefuni.grid(row=i + 1, column=0)
+
 root.mainloop()
